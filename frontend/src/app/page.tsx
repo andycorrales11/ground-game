7eb75758ccf6ce85fe56cb9f5c25bd8c022a2351
Andres Corrales <119633657+andycorrales11@users.noreply.github.com>
2025-06-23 18:36:20 +0000

    nothing

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -7,13 +7,7 @@ export default function Home() {
   return (
     <>
       <Header />
-      <Grid container spacing={2} sx={{ justifyContent: 'center', padding: 2}}>
-        <Grid size={{xs: 6, md: 8}} sx = {{ textAlign: 'center' }}>
-            <h1>Welcome to Ground Game</h1>
-            <p>This dashboard provides advanced statistics for players across various positions, including WRs, QBs, and RBs.</p>
-            <p>Use the navigation to explore player stats and make informed decisions for your fantasy football team.</p>
-        </Grid>
-      </Grid>
+      
     </>
   );
 }
